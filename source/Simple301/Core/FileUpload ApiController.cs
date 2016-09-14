e1611ef12b48f7f6565ad7f5fbad3da6b0a7ba89
Barry Fogarty <barry.fogarty@thisisdare.com>
2016-09-14 16:13:56 +0100

    Add File Upload API for basic uploading function

---
+++ source/Simple301/Core/FileUpload ApiController.cs
@@ -0,0 +1,40 @@
+﻿using Simple301.Core.FileUpload;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Web;
+using System.Web.Http;
+using Umbraco.Web.Mvc;
+using Umbraco.Web.WebApi;
+
+namespace Simple301.Core
+{
+    // If you want this endpoint to only be accessible when the user is logged in, 
+    // then use UmbracoAuthorizedApiController instead of UmbracoApiController
+    [PluginController("Simple301")]
+    public class FileUploadApiController : UmbracoAuthorizedApiController
+    {
+        public async Task<HttpResponseMessage> UploadFileToServer()
+        {
+            if (!Request.Content.IsMimeMultipartContent())
+            {
+                throw new HttpResponseException(HttpStatusCode.UnsupportedMediaType);
+            }
+
+            // Make this directory whatever makes sense for your project.
+            var root = HttpContext.Current.Server.MapPath("~/App_Data/Temp/Simple301");
+            Directory.CreateDirectory(root);
+            var provider = new CustomMultipartFormDataStreamProvider(root);
+            var result = await Request.Content.ReadAsMultipartAsync(provider);
+
+            if (result.FileData.FirstOrDefault() == null)
+                return Request.CreateResponse(HttpStatusCode.InternalServerError, "Error uploading file");
+
+            // Return the full file path to the response
+            return Request.CreateResponse(HttpStatusCode.OK, result.FileData.FirstOrDefault().LocalFileName);
+        }
+    }
+}
---
+++ source/Simple301/Core/FileUpload/CustomMultipartFormDataStreamProvider.cs
@@ -0,0 +1,20 @@
+﻿using System.Net.Http;
+
+namespace Simple301.Core.FileUpload
+{
+    /// <summary>
+    /// Overrides MultipartFormDataStreamProvider so we can derive the original filename fro,m the upload
+    /// See http://www.strathweb.com/2012/08/a-guide-to-asynchronous-file-uploads-in-asp-net-web-api-rtm/
+    /// </summary>
+    public class CustomMultipartFormDataStreamProvider : MultipartFormDataStreamProvider
+    {
+        public CustomMultipartFormDataStreamProvider(string path) : base(path)
+        { }
+
+        public override string GetLocalFileName(System.Net.Http.Headers.HttpContentHeaders headers)
+        {
+            var name = !string.IsNullOrWhiteSpace(headers.ContentDisposition.FileName) ? headers.ContentDisposition.FileName : "NoName";
+            return name.Replace("\"", string.Empty); //this is here because Chrome submits files in quotation marks which get treated as part of the filename and get escaped
+        }
+    }
+}
